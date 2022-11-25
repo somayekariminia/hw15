ce12ba33cd826ed908fa596c409d7816d5619ddf
somaye6868 <somayekariminia6868@gmail.com>
2022-11-25 09:12:24 +0330

    add Education enume

--- src/main/java/model/entity/Student.java
+++ src/main/java/model/entity/Student.java
@@ -1,4 +1,16 @@
 package model.entity;
 
+import java.util.Date;
+
 public class Student {
+    String firstName;
+    String lastName;
+    String nameFather;
+    String nameMother;
+    String nationalCode;
+    String numberIdentity;
+    Date birthday;
+    String numberStudent;
+    University university;
+
 }
---
+++ src/main/java/model/enumes/TypeUniversity.java
@@ -0,0 +1,4 @@
+package model.enumes;
+
+public enum TypeUniversity {
+}
