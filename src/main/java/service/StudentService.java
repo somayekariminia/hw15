27bc4c569016962d01e06b14369b74c667334759
somaye6868 <somayekariminia6868@gmail.com>
2022-11-26 22:05:58 +0330

    add StudentserviceImpl class

---
+++ src/main/java/service/StudentService.java
@@ -0,0 +1,30 @@
+package service;
+
+import model.entity.Person;
+import model.entity.Student;
+
+public class StudentService {
+
+    public void signUp() {
+        //todo
+    }
+
+    public Person signIn(String userName, String password) {
+        //todo
+        return null;
+    }
+
+    public boolean RequestLoan(Student student) {
+        //todo
+        return false;
+    }
+
+    public void registerLoan(Student student) {
+        //todo
+    }
+
+    public boolean graduate(Student student) {
+        //todo
+        return false;
+    }
+}
