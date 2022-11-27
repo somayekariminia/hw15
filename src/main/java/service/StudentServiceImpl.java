5bd6ed7ba67492718794eee18aa77b310dd49cd1
somaye6868 <somayekariminia6868@gmail.com>
2022-11-27 12:58:41 +0330

    add AllocatedLoan class

--- src/main/java/service/StudentServiceImpl.java
+++ src/main/java/service/StudentServiceImpl.java
@@ -3,11 +3,12 @@ package service;
 import model.entity.Person;
 import model.entity.Student;
 
-public class StudentService {
+public class StudentServiceImpl {
 
     public void signUp() {
         //todo
     }
+
 
     public Person signIn(String userName, String password) {
         //todo
